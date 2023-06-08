05a145734a3bb637fbbd50329aabf156e8836fda
ChaimaeTIBOUDA <125551991+ChaimaeTIBOUDA@users.noreply.github.com>
2023-06-08 21:56:54 +0000

    python

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-def magic_calculation(a, b):
+def magic_calculation(x, y):
     from magic_calculation_102 import add, sub
-    if a < b:
-        c = add(a, b)
+    if x < y:
+        j = add(x, y)
         for i in range(4, 6):
-            c = add(c, i)
-        return c
+            j = add(j, i)
+        return j
     else:
-        return sub(a, b)
+        return sub(x, y)
