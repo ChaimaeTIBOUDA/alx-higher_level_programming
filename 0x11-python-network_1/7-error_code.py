8b8fd2703dcf687254190529315d78483b9005ad
ChaimaeTIBOUDA <125551991+ChaimaeTIBOUDA@users.noreply.github.com>
2023-09-29 21:17:56 +0000

    python network

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,13 +1,17 @@
-#!/usr/bin/python3
-""" Show status code in request """
-
+   #!/usr/bin/python3
+"""Sends a request to a given URL and displays the response body.
+Usage: ./7-error_code.py <URL>
+  - Handles HTTP errors.
+"""
+import sys
 import requests
-from sys import argv
+
 
 if __name__ == "__main__":
-    r = requests.get(argv[1])
+    url = sys.argv[1]
 
-    if 400 > r.status_code:
-        print(r.text)
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
     else:
-        print("Error code: {}".format(r.status_code))~                                                                                  
+        print(r.text)                                                                            
