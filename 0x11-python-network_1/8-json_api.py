8a5faaa53791dc68c9dff56c9cdf8378775550da
ChaimaeTIBOUDA <125551991+ChaimaeTIBOUDA@users.noreply.github.com>
2023-09-29 21:24:31 +0000

    python network

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-
-import requests
+"""A script that
+- takes in a URL
+- sends a request to the URL
+- displays the body of the response.
+"""
 import sys
+import requests
+
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        payload = {'q': sys.argv[1]}
-        r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
-        # if json is not empty and json correct:
-            # print("[{}] <{}>".format(id, name))
-        # elif json incorrect:
-            # print("Not a valid JSON")
-        # else:
-            # print("No result")
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
     else:
-        print("No result")
-    # data = urllib.parse.urlencode({"email": sys.argv[2]})
-    # data = data.encode('ascii')
-    # url = sys.argv[1]
-    # with urllib.request.urlopen(url, data) as response:
-    #     print(response)
+        print(r.text)
